9edd0868ebb06e8c30a4d69a9dd3ca4a5e7c6ddb
Sebastian Londero <xebadevs@gmail.com>
2022-01-07 18:35:35 -0300

    'Footer'

---
+++ files/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer">
+        <h4>&copy; SEBASTIÁN LONDERO 2022</h4>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KeyMe</title>
     <link rel="stylesheet" href="./css/semantic.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
+    <link rel="stylesheet" href="./css/styles.css">
     <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
@@ -23,6 +23,13 @@
     <div>
         <?php require_once('./files/login.php'); ?>
     </div>
+
+    <div class="empty"></div>
+
+    <div>
+        <?php require_once('./files/footer.php'); ?>
+    </div>
+
 
 </body>
 </html>
