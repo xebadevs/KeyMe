c2473bdac3a896bd25449068c168ec818b7a56a4
Sebastian Londero <xebadevs@gmail.com>
2022-01-07 13:27:12 -0300

    'Index.php'

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KeyMe</title>
+    <link rel="stylesheet" href="./css/semantic.min.css">
+    <!--<link rel="stylesheet" href="./css/styles.css">-->
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <div class="empty"></div>
+    <div class="ui grid xd-mt">
+        <div class="z">
+            <h1 class="ui center aligned header xd">KeyMe</h1>
+            <h3>A place to keep your passwords safe</h3>
+        </div>
+    </div>
+</body>
+</html>
