19de5244c66a5d2e5e3be4913a0cec409aa8fae6
Sebastian Londero <xebadevs@gmail.com>
2022-01-07 17:18:36 -0300

    'Update_Index'

---
+++ files/login.php
@@ -0,0 +1,36 @@
+<div class="x-cont-login">
+    <div class="ui column grid">
+        <div class="column">
+            <div class="ui form">
+                <div class="field">
+                    <label>User</label>
+                    <div class="ui left icon input">
+                        <input type="text" placeholder="Username">
+                        <i class="user icon"></i>
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Password</label>
+                    <div class="ui left icon input">
+                        <input type="password" placeholder="Insert your Master Password">
+                        <i class="lock icon"></i>
+                    </div>
+                </div>
+                <div class="xd-df">
+                    <div class="ui inverted primary submit button">
+                        <p>LOGIN</p>
+                    </div>
+                </div>
+                <br>
+                <div class="xd-ma">
+                    <p>
+                        <a href="#">Register</a>
+                    </p>
+                    <p>
+                        <a href="#">I forgot my password</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,16 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KeyMe</title>
     <link rel="stylesheet" href="./css/semantic.min.css">
-    <!--<link rel="stylesheet" href="./css/styles.css">-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
     <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
     <div class="empty"></div>
     <div class="ui grid xd-mt">
-        <div class="z">
+        <div class="x-cont-header">
             <h1 class="ui center aligned header xd">KeyMe</h1>
             <h3>A place to keep your passwords safe</h3>
         </div>
     </div>
+    <div class="empty"></div>
+
+    <div>
+        <?php require_once('./files/login.php'); ?>
+    </div>
+
 </body>
 </html>
