ce11f27d0c92a2c37b8e47cfd678f3eef0a33aa8
Sebastian Londero <xebadevs@gmail.com>
2022-01-14 13:32:41 -0300

    'User

--- files/form-user.php
+++ files/form-user.php
@@ -6,22 +6,22 @@
     $error = NULL;
 
 // VALIDATIONS
-
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $user = trim($_POST['user']);
         $password = trim($_POST['password']);
     }
 
-    if(empty($user) or empty($password) or filter_var($email, FILTER_VALIDATE_EMAIL)){
+    if(empty($user) or empty($password) or !filter_var($user, FILTER_VALIDATE_EMAIL)){
     $error = true;
-    header('location:./error-reg-alt.php');
+    echo 'Wrong data';
+    }else{
+        $encrypt_password = password_hash($password, PASSWORD_BCRYPT);
+
+        $connection = mysqli_connect('localhost', 'root', '', 'keyme');
+        $query = "UPDATE db_users SET user_email = '$user', user_password = '$encrypt_password' WHERE user_email = '$email'";
+        $response = mysqli_query($connection, $query);
+
+        mysqli_close($connection);
+        header('location:./logout.php'); // FOR CHECK
     }
 
-    $encrypt_password = password_hash($password, PASSWORD_BCRYPT);
-
-    $connection = mysqli_connect('localhost', 'root', '', 'keyme');
-    $query = "UPDATE db_users SET user_email = '$user', user_password = '$encrypt_password' WHERE user_email = '$email'";
-    $response = mysqli_query($connection, $query);
-
-    mysqli_close($connection);
-    header('location:./main.php'); // FOR CHECK
