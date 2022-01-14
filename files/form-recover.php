741d79fae1112e3aad3c98f5d3cc787660df75e9
Sebastian Londero <xebadevs@gmail.com>
2022-01-13 23:15:59 -0300

    'Recover_password'

--- files/form-recover.php
+++ files/form-recover.php
@@ -12,6 +12,7 @@
         $connection = mysqli_connect('localhost', 'root', '', 'keyme');
         $query = "SELECT user_password FROM db_users WHERE user_email = '$email' LIMIT 1";
         $response = mysqli_query($connection, $query);
+        mysqli_close($connection);
 
         $row = mysqli_fetch_row($response);
         $password = $row[0];
@@ -21,11 +22,16 @@
             header('location:./error-user.php'); // USER DIDN'T EXISTS IN DDBB
         } else {
 
-            //        NEW IMPLEMENTATION
+            // HASH GENERATION AND UPDATE OF NEW RANDOM PASSWORD IN DDBB
             $values = 'zxcvbnmasdfghjklqewrtyuiop0123456789';
-            $random_value = substr(str_shuffle($values), 0, 15);
-            $random_hash = password_hash($random_value, PASSWORD_BCRYPT);
+            $random_password = substr(str_shuffle($values), 0, 15);
+            $random_hash = password_hash($random_password, PASSWORD_BCRYPT);
 
+            $connection = mysqli_connect('localhost', 'root', '', 'keyme');
+            $query_hash = "UPDATE db_users SET user_password = '$random_hash' WHERE user_email = '$email'";
+            $res_hash = mysqli_query($connection, $query_hash);
+
+            // PHPMAILER
             $mail = new PHPMailer(true);
 
             try {
@@ -46,8 +52,8 @@
                 //Content
                 $mail->isHTML(true);
                 $mail->Subject = 'Test message';
-                $mail->Body = "<p>You have required your <b>Master Password</b>, wich is:</p> <h3 style='color:red;'>$password</style></h3><p>Please save it in a highly secure place. Now you can access from:</p><p>http://localhost/projects/portfolio/keyme/KeyMe/index.php</p>";
-                 $mail->AltBody = "You have required your Master Password, wich is: [$password]. Please save it in a highly secure place. Now you can access from: http://localhost/projects/portfolio/keyme/KeyMe/index.php";
+                $mail->Body = "<p>You have required your <b>Master Password</b>, wich is:</p> <h3 style='color:red;'>$random_password</style></h3><p>Please save it in a highly secure place. Now you can access from:</p><p>http://localhost/projects/portfolio/keyme/KeyMe/index.php</p>";
+                 $mail->AltBody = "You have required your Master Password, wich is: [$random_password]. Please save it in a highly secure place. Now you can access from: http://localhost/projects/portfolio/keyme/KeyMe/index.php";
 
                 $mail->send();
                 echo 'Message has been sent';
@@ -57,5 +63,5 @@
             }
         }
     }else{
-    header('location:./error-mail.php'); // INVALID E-MAIL
+        header('location:./error-mail.php'); // INVALID E-MAIL
 }
