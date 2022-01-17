90ed1d3a5c091d73a1ca6b316e6c5bddd676d841
Sebastian Londero <xebadevs@gmail.com>
2022-01-17 15:31:23 -0300

    Hosting_connection

--- files/connection.php
+++ files/connection.php
@@ -1,3 +1,8 @@
 <?php
-
+    // XAMPP CONNECTION
     $connection = mysqli_connect('localhost', 'root', '', 'keyme');
+
+    // HOSTING CONNECTION
+//    $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348', '
+//nOa5Bcem97WbYR', 'epiz_30837348_keyme');
+?>
