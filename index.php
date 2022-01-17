773c606e850a95d1e4385e6fd04c336cdc89b7e1
Sebastian Londero <xebadevs@gmail.com>
2022-01-16 23:26:59 -0300

    'About'

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <div class="x-cont-header">
             <h1 class="ui center aligned header xd">KeyMe</h1>
             <h3>A place to keep your passwords safe</h3>
-            <a href="#" class="read">Read more</a>
+            <a href="./files/about.php" class="read">Read more</a>
         </div>
     </div>
     <div class="empty"></div>
