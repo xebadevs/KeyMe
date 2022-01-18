b58e0042d9c66ba5ff9bbcf3422a513b7b775e64
Sebastian Londero <xebadevs@gmail.com>
2022-01-18 12:15:29 -0300

    About

--- files/about.php
+++ files/about.php
@@ -14,33 +14,42 @@
 <body>
 <div class="empty"></div>
 <div class="ui grid xd-mt">
-    <div class="x-cont-header">
+    <div class="x-cont-header-abt">
         <h1 class="ui center aligned header xd">KeyMe</h1>
-        <h3>A place to keep your passwords safe</h3>
+        <h3>Our service provides you with the opportunity to keep your passwords safe</h3>
+        <h3>We apply encryption / decryption methods and different validations.</h3>
+        <h3>Light and easy to use. Just...</h3>
     </div>
-</div>
-<div class="empty"></div>
-
-<div>
-    <i class="address card outline icon"></i>
-    <i class="shield alternate icon"></i>
-    <i class="save outline icon"></i>
-    <i class="keyboard outline icon"></i>
 </div>
 
 <div class="cont-about">
     <div class="about">
-            <div class="box a">A</div>
-            <div class="box b">B</div>
-            <div class="box c">C</div>
-            <div class="box d">D</div>
+            <div class="box a">
+                <i class="address card outline icon"></i>
+                <h3>1. Create an <br> account</h3>
+            </div>
+            <div class="box b">
+                <i class="shield alternate icon"></i>
+                <h3>2. Define your <br> master password</h3>
+            </div>
+            <div class="box c">
+                <i class="save outline icon"></i>
+                <h3>3. Start saving <br> your data.</h3>
+            </div>
+            <div class="box d">
+                <i class="keyboard outline icon"></i>
+                <h3>4. Look for it <br> when you need it</h3>
+            </div>
     </div>
 </div>
 
-<div class="empty"></div>
+
+<div class="xd-df">
+    <button class="ui inverted primary submit button">
+        <p>LOGIN</p>
+    </button>
+</div>
 
 <div>
     <?php require_once('./footer.php'); ?>
 </div>
-</body>
-</html>
--- files/connection.php
+++ files/connection.php
@@ -2,6 +2,9 @@
     // XAMPP CONNECTION
 //    $connection = mysqli_connect('localhost', 'root', '', 'keyme');
 
-    // HOSTING CONNECTION
-        $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348', '
-    nOa5Bcem97WbYR', 'epiz_30837348_keyme');
+    // INFINITYFREE HOSTING
+//        $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348', '
+//nOa5Bcem97WbYR', 'epiz_30837348_keyme');
+
+    // 000WEBHOST
+    $connection = mysqli_connect('localhost', 'id18303852_xebadevs', 'uK3f0Zz@V)x4jUMu', 'id18303852_keyme');
