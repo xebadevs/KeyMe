4f8e26a636e174fa2d0c6da9507981440d585d19
Sebastian Londero <xebadevs@gmail.com>
2022-01-19 13:17:43 -0300

    Connection_fix

--- files/connection.php
+++ files/connection.php
@@ -3,5 +3,5 @@
 //    $connection = mysqli_connect('localhost', 'root', '', 'keyme');
 
     // INFINITYFREE HOSTING
-        $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348', '
-nOa5Bcem97WbYR', 'epiz_30837348_keyme', 3306);
+        $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348',
+            'nOa5Bcem97WbYR', 'epiz_30837348_keyme', 3306);
