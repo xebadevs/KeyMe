75a26e67552fedb04aec2cd2c051d0e6345d227d
Sebastian Londero <xebadevs@gmail.com>
2022-01-27 23:35:12 -0300

    Edit_password

--- files/form-user.php
+++ files/form-user.php
@@ -8,23 +8,19 @@
 
     // VALIDATIONS
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $user = trim($_POST['user']);
         $password = trim($_POST['password']);
     }
 
-    $user_schar = preg_match_all($s_chars, $user);
     $password_schar = preg_match_all($s_chars, $password);
 
-    if(empty($user) or empty($password) or !filter_var($user, FILTER_VALIDATE_EMAIL)){
-        header('location:./error-edituser.php');
-    }elseif($user_schar > 0 or $password_schar > 0){
+    if(empty($password)){
         header('location:./error-edituser.php');
     }else {
         $encrypt_password = password_hash($password, PASSWORD_BCRYPT);
 
         require_once('connection.php');
         global $connection;
-        $query = "UPDATE db_users SET user_email = '$user', user_password = '$encrypt_password' WHERE user_email = '$email'";
+        $query = "UPDATE db_users SET user_password = '$encrypt_password' WHERE user_email = '$email'";
         $response = mysqli_query($connection, $query);
 
         mysqli_close($connection);
