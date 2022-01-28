8f79a2d21431ed5615cb122be64e548124dc2ddd
Sebastian Londero <xebadevs@gmail.com>
2022-01-27 22:13:31 -0300

    Title_font

--- files/connection.php
+++ files/connection.php
@@ -1,7 +1,7 @@
 <?php
     // XAMPP CONNECTION
-//    $connection = mysqli_connect('localhost', 'root', '', 'keyme');
+    $connection = mysqli_connect('localhost', 'root', '', 'keyme');
 
     // INFINITYFREE HOSTING
-        $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348',
-            'nOa5Bcem97WbYR', 'epiz_30837348_keyme', 3306);
+//        $connection = mysqli_connect('sql104.epizy.com', 'epiz_30837348',
+//            'nOa5Bcem97WbYR', 'epiz_30837348_keyme', 3306);
--- files/edit.php
+++ files/edit.php
@@ -33,13 +33,14 @@
     <link rel="stylesheet" href="../css/semantic.min.css">
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/addedit.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap">
 </head>
 
 <body>
     <div class="empty"></div>
     <div class="ui grid xd-mt">
         <div class="x-cont-header">
-            <h1 class="ui center aligned header xd">
+            <h1 class="ui center aligned header xd xd-title">
                 <a href="main.php">KeyMe</a>
             </h1>
         </div>
