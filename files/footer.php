ed7524d80566664f9a7ee4096b6904e5c5136e83
Sebastian Londero <xebadevs@gmail.com>
2022-02-07 23:57:03 -0300

    Adding_LinkedIn

--- files/footer.php
+++ files/footer.php
@@ -1,5 +1,9 @@
 <footer>
     <div class="footer">
-        <h4>&copy; SEBASTIÁN LONDERO 2022</h4>
+        <a href="" class="footer-color">
+            <h4>
+                &copy; SEBASTIÁN LONDERO 2022
+            </h4>
+        </a>
     </div>
 </footer>
